5bb152ff576e70f93c4bb083facba248bb9ef718
TulgaAnkh <kekerhonor3@gmail.com>
2020-07-18 11:11:59 +0900

    first commit

---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def post(request):
+    posts = [
+        {
+            "title": "Post1",
+            "text": "ahdshba",
+            "created": "2020-07-18 10:39"
+        },{
+            "title": "Post2",
+            "text": "buterbrod s sirom",
+            "created": "2020-07-18 21:14"
+        },{
+            "title": "Post2",
+            "text": "asdasd",
+            "created": "2020-07-17 14:19"
+        }
+    ]
+    return render(request, "post.html", { "data" : posts })
+def index(request):
+    return render(request, "index.html")
+def comment(request):
+    return render(request, "comment.html")
+def about(request):
+    return render(request, "about.html")
